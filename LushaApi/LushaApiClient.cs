9e7567ae62b2991c1f71b0fb30987d112490b782
░▒▓█│【Walkman】│█▓▒░ <matthewc@direwolf.co.za>
2026-07-17 16:00:50 +0200

    changed to file-scoped namespaces & added CallerArgumentExpression/Nullable attribute polyfills, file-scoped namespaces requires C# 10.0

--- LushaApi/LushaApiClient.cs
+++ LushaApi/LushaApiClient.cs
@@ -2,21 +2,20 @@ using System.Text.Json;
 using RestSharp;
 using RestSharp.Serializers.Json;
 
-namespace LushaApi {
+namespace LushaApi;
 
-    public interface ILushaApiClient {
-    }
+public interface ILushaApiClient {
+}
 
-    public class LushaApiClient : ILushaApiClient {
-        private readonly RestClient client;
+public class LushaApiClient : ILushaApiClient {
+    private readonly RestClient client;
 
-        public LushaApiClient(string apikey) {
-            Utils.Utils.RequireStringArgument(apikey, nameof(apikey));
+    public LushaApiClient([System.Diagnostics.CodeAnalysis.AllowNull] string apikey) {
+        Utils.Utils.RequireStringArgument(apikey);
 
-            client = new RestClient("https://api.lusha.com/v3/", configureSerialization: config => config.UseSystemTextJson(new JsonSerializerOptions(JsonSerializerDefaults.Web)));
+        client = new RestClient("https://api.lusha.com/v3/", configureSerialization: config => config.UseSystemTextJson(new JsonSerializerOptions(JsonSerializerDefaults.Web)));
 
-            client.AddDefaultHeader("Accept", "application/json");
-            client.AddDefaultHeader("api_key", apikey);
-        }
+        client.AddDefaultHeader("Accept", "application/json");
+        client.AddDefaultHeader("api_key", apikey);
     }
 }
--- LushaApi/Utils/Utils.cs
+++ LushaApi/Utils/Utils.cs
@@ -1,19 +1,26 @@
 using System;
 
-namespace LushaApi.Utils {
+namespace LushaApi.Utils;
 
-    internal class Utils {
-        /// <summary>Require a parameter to not be null</summary>
-        /// <param name="argument">Value of the parameter</param>
-        /// <param name="paramName">Name of the parameter</param>
-        public static void RequireArgument(object? argument, string paramName) {
-            if (argument == null)
-                throw new ArgumentNullException(paramName);
-        }
+internal class Utils {
+    /// <summary>Require a parameter to not be null</summary>
+    /// <param name="argument">Value of the parameter</param>
+    /// <param name="paramName">Name of the parameter</param>
+    public static void RequireArgument(
+        [System.Diagnostics.CodeAnalysis.NotNull] object? argument,
+        [System.Runtime.CompilerServices.CallerArgumentExpression(nameof(argument))] string? paramName = null
+    ) {
+        if (argument == null)
+            throw new ArgumentNullException(paramName);
+    }
 
-        public static void RequireStringArgument(string? argument, string paramName) {
-            if (string.IsNullOrWhiteSpace(argument))
-                throw new ArgumentNullException(paramName);
-        }
+    public static void RequireStringArgument(
+        [System.Diagnostics.CodeAnalysis.NotNull] string? argument,
+        [System.Runtime.CompilerServices.CallerArgumentExpression(nameof(argument))] string? paramName = null
+    ) {
+        if (string.IsNullOrWhiteSpace(argument))
+            throw new ArgumentNullException(paramName);
+        if (argument == null) // satisfy warning
+            throw new ArgumentNullException(paramName);
     }
 }
